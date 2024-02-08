85d484cd76486fdd17d90d9dc7e825279a8dc5a8
Antoni061 <ezapatero939@gmail.com>
2024-02-07 22:05:51 -0600

    version2

---
+++ src/app/componentes/register/register.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../servicios/auth.service';
+import { User } from '../../interfaces/user';
+import { response } from 'express';
+import { error } from 'console';
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  registerForm=this.fb.group({
+    fullname:['', [Validators.required]],
+    email:['',[Validators.required, Validators.email]],
+    password:['',Validators.required],
+    confirmPassword:['',Validators.required]
+  });
+
+  constructor(private fb:FormBuilder,
+    private authService:AuthService,
+    private messageService:MessageService
+    ){
+
+  }
+
+  get email(){
+    return this.registerForm.controls['email']
+  }
+
+  get fullname(){
+    return this.registerForm.controls['fullname']
+  }
+
+  get password(){
+    return this.registerForm.controls['password']
+  }
+
+  get confirmPassword(){
+    return this.registerForm.controls['confirmPassword']
+  }
+
+  enviarUsuario(){
+    console.log("Estoy enviando datos")
+
+    const datos = { ...this.registerForm.value}
+    delete datos.confirmPassword;
+
+    this.authService.registerUser(datos as User).subscribe(
+      response=>{
+        this.messageService.add({
+          severity:'success',
+          summary:'Registro Exitoso',
+          detail:'El usuario ha sida registrado con Exito'
+        });
+      },
+      error=>{
+
+      }
+    )
+  }
+}
---
+++ src/app/servicios/auth.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private  baseUrl="http://localhost:3000"
+
+  constructor(private http:HttpClient) { }
+
+  registerUser(usuario:User){
+    return this.http.post(`${this.baseUrl}/users`,usuario);
+  }
+}
